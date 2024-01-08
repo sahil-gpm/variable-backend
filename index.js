b19ac67f79068305cda1dc9896ca88d22bfe737c
sahil-gpm <sahilchavangithub@gmail.com>
2024-01-08 13:15:37 +0530

    change in index.js : changed cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 
 //middlewares
-app.use(cors({ methods: ["GET", "POST"], origin: "http://localhost:3000" }))
+app.use(cors())
 app.use(express.json())
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ parameterLimit: 1000000000000000, limit: '500mb', extended: true }));
